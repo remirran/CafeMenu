dc1ce673d90b5747e474d647098478506ed97917
Ilya G. Teterev <remirran@gmail.com>
2012-06-27 01:12:20 +0400

    Table alignment

--- src/com/remirran/digitalmenu/data/FileCache.java
+++ src/com/remirran/digitalmenu/data/FileCache.java
@@ -23,14 +23,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuff.Mode;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.net.http.AndroidHttpClient;
 import android.util.Log;
 import android.widget.ImageView;
